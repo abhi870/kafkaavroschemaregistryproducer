0416d8c288ba126f741d926e2e72df609fe972e5
abhi870 <abhishekpaithane@gmail.com>
2021-08-19 21:46:16 +0530

    working producer

---
+++ src/main/java/com/avroproducer/producer/ProducerApplication.java
@@ -0,0 +1,33 @@
+package com.avroproducer.producer;
+
+import com.local.User;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Autowired
+	private KafkaProducer kafkaProducer;
+	@Bean
+	public CommandLineRunner demo(){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				User user = User.newBuilder().setFirstName("Abhishek").setLastName("Paithane").build();
+				ProducerRecord record = new ProducerRecord("test12345","key123", user);
+				kafkaProducer.send(record);
+			}
+		};
+	}
+}
---
+++ src/main/java/com/avroproducer/producer/config/KafkaConfigs.java
@@ -0,0 +1,26 @@
+package com.avroproducer.producer.config;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KafkaConfigs {
+    @Bean
+    public KafkaProducer getKafkaProducer(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        properties.put("use.latest.version", true);
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+        return kafkaProducer;
+    }
+}
